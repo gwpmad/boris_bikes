03bd2dded720957ec32f2cd03cc0bd8bcff3ea72
samover <samuel.overloop@gmail.com>
2015-10-13 21:25:28 +0100

    cleaned out the code

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,26 +1,27 @@
 require 'docking_station'
-#require 'bike'
-#station = DockingStation.new
 
 describe DockingStation do
 	it { is_expected.to respond_to(:release_bike) }
 
-   it 'releases a working bike' do
-     bike = Bike.new
-     subject.dock(bike)
-     bike = subject.release_bike
-     expect(bike).to be_working
-	 end
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+    it 'throws and error docking station is empty' do
+      expect {subject.release_bike}.to raise_error(StandardError)
+    end
+	end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-	it 'docks an object of the Bike class as an instance variable' do
-		docked_bike = Bike.new
-		subject.dock(docked_bike)
-		expect(subject.bike).to eq docked_bike
-	end
-
-  it 'throws and error when #release_bike when #subject.bikes is nil' do
-    expect {subject.release_bike}.to raise_error(StandardError)
+  describe '#dock' do
+    it 'docks a bike and returns the bike' do
+      docked_bike = Bike.new
+      subject.dock(docked_bike)
+      expect(subject.bike).to eq docked_bike
+    end
   end
+
 end
