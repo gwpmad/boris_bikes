4fcc73300e081b50007c429f3a1264a3d8e0c4a9
samover <samuel.overloop@gmail.com>
2015-10-13 16:25:13 +0100

    updated challenge 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,12 @@
 require 'docking_station'
-
+#require 'bike'
 #station = DockingStation.new
 
-describe DockingStation do 
+describe DockingStation do
 	it { is_expected.to respond_to(:release_bike) }
+
+  it 'releases a working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
